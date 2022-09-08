6a389bc5064af3c125cf0e62e9e0c20c65dca82d
L_Rock <78535614+L-Rock-C@users.noreply.github.com>
2022-09-08 03:08:14 -0300

    Get instructions and labels

---
+++ example2.asm
@@ -0,0 +1,8 @@
+	li $t0, 75
+	addi $t1, $zero, -25
+	add $s0, $t0, $t1
+	addi $s1, $s0, 36
+	
+	
+	li $a0, 10
+	syscall
--- main.py
+++ main.py
@@ -1,39 +1,69 @@
-L1: add $t0, $s1, $s2
-L2: addi $t1, $s3, 7
-	beq $t0, $t1, L1
-	j L2
-
 # Read file
-file = open('example1.asm', 'r')
+file = open('example3.asm', 'r')
 lines = file.readlines()
+
 
 # Variables
 labels = list()
 labelsIndex = list()
 
-# Get labels
-# Run through each line
-def getLabel():
-    count = 0
+instructions = list()
+instructionsIndex = list()
+
+
+def getLines():
+    address = 0
     for line in lines:
-        # Check if there's a label
         if line.find(":") > 0:
-            tempStr = ""
-            # Set the label value and index
-            for letter in line:
-                tempStr += letter
-                if letter == ":":
-                    labels.append(tempStr.strip(":"))
-                    labelsIndex.append(count)
-                    break
-        count += 1
+            labelStr = ""
+            instrStr = ""
+            endLabel = False
+            # Set the value and index of labels and instructions
+            for letter in line.strip():
+                if not endLabel:
+                    if letter == ":":
+                        labels.append(labelStr)
+                        labelsIndex.append(address)
+                        endLabel = True
+                    else:
+                        labelStr += letter
+                else:
+                    instrStr = line.strip(labelStr)
+                    labelStr += ":"
+
+            instructions.append(instrStr.strip())
+            instructionsIndex.append(address)
+        else:
+            instructions.append(line.strip())
+            instructionsIndex.append(address)
+        address += 1
 
 
-def getInstruction():
-    for line in lines:
-        
+def runAssembler():
+    i = 0
+    binCode = ""
+    while i < len(instructionsIndex):
+        for tag in instructions[i].split():
+            if tag == "sll":
+                opcode = "000000"
+                binCode += opcode
+            if tag == "$s0,":
+                rs = "01001"
+                binCode += rs
+            if tag == "$s1,":
+                rt = "01010"
+                binCode += rt
+            if tag == "$s2,":
+                rd = "01000"
+                binCode += rd
+            if tag == int:
+                sa = bin(int(tag))
+                binCode += sa
+            func = "100000"
+            binCode += func
 
+        i += 1
+    print(binCode)
 
-getLabel()
-
-
+getLines()
+runAssembler()
